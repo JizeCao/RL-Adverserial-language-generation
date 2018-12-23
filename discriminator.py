3877a9ad8fe4f24ffba5cc1c3127bdf9cdb93435
Jize Cao <caojize@barb.cs.washington.edu>
2018-12-22 21:41:37 -0800

    d

--- discriminator.py
+++ discriminator.py
@@ -5,13 +5,15 @@ import random
 import torch.nn as nn
 from torch import optim
 import time
-from search_utils import tensorFromPair, trim_dummy_sen, load_model_dictionary_pairs, logging, Voc
+from search_utils import tensorFromPair, trim_dummy_sen, load_model_dictionary_pairs, logging
 from model import hierEncoder_frequency
 import argparse
 
 parser = argparse.ArgumentParser(description='UCT based on the language model')
-parser.add_argument('--save_dir', type=str, default='../data/save',
+parser.add_argument('--save_dir', type=str, default='./data/save',
                     help='directory of the save place')
+parser.add_argument('--retrain', action='store_true', 
+                    help='retrain from an existing checkpoint')
 
 
 args = parser.parse_args()
@@ -19,6 +21,53 @@ args.gen_lr = 0
 args.dis_lr = 0.001
 args.cuda = True if torch.cuda.is_available() else False
 args.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+class Voc:
+    def __init__(self, name):
+        self.name = name
+        self.trimmed = False
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
+        self.num_words = 3  # Count SOS, EOS, PAD
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+
+    # Remove words below a certain count threshold
+    def trim(self, min_count):
+        if self.trimmed:
+            return
+        self.trimmed = True
+
+        keep_words = []
+
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+
+        print('keep_words {} / {} = {:.4f}'.format(
+            len(keep_words), len(self.word2index), len(keep_words) / len(self.word2index)
+        ))
+
+        # Reinitialize dictionaries
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: "PAD", SOS_token: "SOS", EOS_token: "EOS"}
+        self.num_words = 3 # Count default tokens
+
+        for word in keep_words:
+            self.addWord(word)
 
 
 def pretrainD(modelD, TrainSet, GenSet, EOS_token, vocabulary, learning_rate=0.001, batch_size=128, to_device=True):
