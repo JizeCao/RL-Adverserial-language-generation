bc6dd61e24676d2109da521ad097fd6273365c43
JizeCao <caojize@uw.edu>
2018-12-26 04:44:21 -0800

    add

--- parallel_generation.sh
+++ parallel_generation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-screen -d -m python UCT_generation.py --gpu_id 4 --RL_index 321 --begin 0 -val_begin 0 --batch_size 10000 --val_batch_size 1000
-screen -d -m python UCT_generation.py --gpu_id 5 --RL_index 321 --begin 10000 -val_begin 1000 --batch_size 10000 --val_batch_size 1000
-screen -d -m python UCT_generation.py --gpu_id 6 --RL_index 321 --begin 20000 -val_begin 2000 --batch_size 10000 --val_batch_size 1000
+echo 'Generate'
+screen -d -m python UCT_generation.py --gpu_id 0 --RL_index 321 --begin 0 --val_begin 0 --batch_size 10000 --val_batch_size 1000 --cuda
+screen -d -m python UCT_generation.py --gpu_id 5 --RL_index 321 --begin 10000 --val_begin 1000 --batch_size 10000 --val_batch_size 1000 --cuda
+screen -d -m python UCT_generation.py --gpu_id 7 --RL_index 321 --begin 20000 --val_begin 2000 --batch_size 10000 --val_batch_size 1000 --cuda
